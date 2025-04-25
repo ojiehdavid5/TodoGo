d05b2df125a242b760ab8dec57883ff00a859335
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-25 22:00:19 +0100

    having issues connecting with the database

--- main.go
+++ main.go
@@ -53,14 +53,56 @@ func main() {
 }
 
 func indexHandler(c *fiber.Ctx, db *sql.DB) error {
-	return c.SendString("Hello")
+	// return c.SendString("Hello")
+
+	var res string
+   var todos []string
+   rows, err := db.Query("SELECT * FROM todos")
+   defer rows.Close()
+   if err != nil {
+       log.Fatalln(err)
+       c.JSON("An error occured")
+   }
+   for rows.Next() {
+       rows.Scan(&res)
+       todos = append(todos, res)
+   }
+   return c.Render("index", fiber.Map{
+       "Todos": todos,
+   })
+}
+type todo struct {
+	Item string `json:"item"`
 }
 func postHandler(c *fiber.Ctx,db *sql.DB) error {
-	return c.SendString("Hello")
-}
-func putHandler(c *fiber.Ctx,db *sql.DB) error {
-	return c.SendString("Hello")
+		newTodo := todo{}
+		if err := c.BodyParser(&newTodo); err != nil {
+			log.Printf("An error occured: %v", err)
+			return c.SendString(err.Error())
+		}
+		fmt.Printf("%v", newTodo)
+		if newTodo.Item != "" {
+			_, err := db.Exec("INSERT into todos VALUES ($1)", newTodo.Item)
+			if err != nil {
+				log.Fatalf("An error occured while executing query: %v", err)
+			}
+		}
+	 
+		return c.Redirect("/")
+	 }
+	 
+
+func putHandler(c *fiber.Ctx,db *sql.DB) error {olditem := c.Query("olditem")
+newitem := c.Query("newitem")
+db.Exec("UPDATE todos SET item=$1 WHERE item=$2", newitem, olditem)
+return c.Redirect("/")
+
 }
 func deleteHandler(c *fiber.Ctx,db *sql.DB) error {
-	return c.SendString("Hello")
+	item := c.Query("item")
+	_, err := db.Exec("DELETE FROM todos WHERE item=$1", item)
+	if err != nil {
+		log.Fatalf("An error occured while executing query: %v", err)
+	}
+	return c.Redirect("/")
 }
